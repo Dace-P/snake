f358480541cac7e02d63a0a4d4fe4945a8669b6f
Dave Park <dave.park@tcu.edu>
2024-02-21 18:23:34 -0600

    Setting up board to render in

--- board.py
+++ board.py
@@ -1,2 +1,18 @@
 class board:
-    print
+    height = 0
+    width = 0
+
+    def __init__(self, height, width):
+        # TODO : Implement sizes according to the provided parameters
+        self.height = height
+        self.width = width
+        print("This be working")
+
+    def render(self):
+        # TODO: Initialize the size of the 
+        print("The height is: " + str(self.height))
+        print("The width is: " + str(self.width))
+        print("This also be working")
+
+board = board(10,15)
+board.render()
