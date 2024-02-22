86f338c1e976903ff9561562e6147c737804afff
Dave Park <dave.park@tcu.edu>
2024-02-22 11:27:03 -0600

    Head function set up

--- snake.py
+++ snake.py
@@ -1,7 +1,16 @@
 class snake:
-    def __init__(self, body, direction):
-        self.body = body
-        self.direction = direction
+    def __init__(self, headx, heady):
+        self.body = [[headx, heady]]
+        #self.direction = direction
     
-    def take_step():
+    def take_step(self):
         print()
+
+    def set_direction(self):
+        print()
+    
+    def head(self):
+        return self.body[0]
+    
+snake = snake(1,2)
+print(snake.head())
