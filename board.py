76729a8ddea11758e1189c6c414cf74be069a198
Dave Park <dave.park@tcu.edu>
2024-02-21 18:36:25 -0600

    Update

--- board.py
+++ board.py
@@ -3,16 +3,21 @@ class board:
     width = 0
 
     def __init__(self, height, width):
-        # TODO : Implement sizes according to the provided parameters
+        # TODO : Define variables as needed
         self.height = height
         self.width = width
+        self.board = []
         print("This be working")
 
+    def boardmatrix(self):
+        self.board = [[None]*self.width]*self.height
+
     def render(self):
-        # TODO: Initialize the size of the 
+        # TODO: Initialize the board and display on the commandline
         print("The height is: " + str(self.height))
         print("The width is: " + str(self.width))
         print("This also be working")
 
 board = board(10,15)
 board.render()
+board.boardmatrix()
